fef9a48c543f5072bc02d68dbce0db8ea0dfc154
ATOIsus <sabinmaharjan884@gmail.com>
2023-05-31 16:16:16 +0545

    Data Time and Dependency Injection done.

---
+++ Flutter (Mobile App Development)/flutter_applications/date_time_example/android/app/src/main/kotlin/com/softwarica/app/date_time_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.softwarica.app.date_time_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Flutter (Mobile App Development)/flutter_applications/date_time_example/lib/repository/user_repository.dart
@@ -0,0 +1,40 @@
+import '../data_source/local/local_data_source.dart';
+import '../data_source/remote/remote_data_source.dart';
+import '../model/user.dart';
+
+class UserRepository {
+  LocalDataSource localSource;
+  RemoteDataSource remoteSource;
+
+  UserRepository({required this.localSource, required this.remoteSource});
+
+  bool hasNetwork = false;
+
+  bool addUser(User user) {
+    bool isAdded = false;
+    if (hasNetwork) {
+      isAdded = remoteSource.addUser(user);
+    } else {
+      isAdded = localSource.addUser(user);
+    }
+    return isAdded;
+  }
+
+  List<User> getUsers() {
+    if (hasNetwork) {
+      return remoteSource.getUsers();
+    } else {
+      return localSource.getUsers();
+    }
+  }
+}
+
+
+//DI: Dependency Injection
+//get_it
+//Service locator
+//Create one instance.
+//Reuse the same instance.
+
+
+//Dependency Rule: Upper is dependent on lower.
