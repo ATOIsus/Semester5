82fc069cef32004d9e9615f9fea48c9d9686ce4a
ATOIsus <sabinmaharjan884@gmail.com>
2023-06-22 14:00:48 +0545

    Testing done.

---
+++ Web API/JavaScript/Day_16/test/models/User.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        minLength: 6,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    fullname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    }
+})
+
+// set toJSON method to not to return hashed password
+
+userSchema.set('toJSON', {
+    transform: (document, returnedDocument) => {
+        returnedDocument.id = document._id.toString()
+        delete returnedDocument._id
+        delete returnedDocument.password
+        delete returnedDocument.__v
+    }
+})
+
+module.exports = new mongoose.model('User', userSchema)
