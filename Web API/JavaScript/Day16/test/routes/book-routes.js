5c1fe0797cc2904047b3dd32d8dfc0b6be38d2e4
ATOIsus <sabinmaharjan884@gmail.com>
2023-06-28 11:49:01 +0545

    Testing of API done.

---
+++ Web API/JavaScript/Day16/test/routes/book-routes.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const router = express.Router()
+const bookController = require('../controllers/book-controller')
+const reviewController = require('../controllers/review-controller')
+const { verifyAdmin } = require('../middlewares/auth')
+
+router.route('/')
+    .get(bookController.getAllBooks)
+    .post(verifyAdmin, bookController.createBook)
+    .put((req, res) => {
+        res.status(405).json({ error: "PUT request is not allowed" })
+    })
+    .delete(verifyAdmin, bookController.deleteAllBooks)
+
+router.route('/:book_id')
+    .get(bookController.getBookById)
+    .post((req, res) => {
+        res.status(405).json({ error: 'POST request is not allowed' })
+    })
+    .put(bookController.updateBookById)
+    .delete(bookController.deleteBookById)
+
+router.route('/:book_id/reviews')
+    .get(reviewController.getAllReviews)
+    .post(reviewController.createReview)
+    .put((req, res) => {
+        res.status(405).json({ error: "PUT request is not allowed" })
+    })
+    .delete(reviewController.deleteAllReviews)
+
+router.route('/:book_id/reviews/:review_id')
+    .get(reviewController.getReviewById)
+    .put(reviewController.updateReviewById)
+    .delete(reviewController.deleteReviewById)
+    .post((req, res) => {
+        res.status(405).json({ error: 'POST request is not allowed' })
+    })
+module.exports = router
